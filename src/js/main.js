aa5eafab18497de0b17a1cbbef225967e71cb65c
Hugo Hendrix Carvalho Rosa <75282590+HugoHendrix@users.noreply.github.com>
2024-10-26 15:56:33 -0300

    adicionado rodape

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,20 @@ images.forEach(image => {
     image.setAttribute('alt', brandName);
     image.setAttribute('title', brandName);
 });
+
+// Galeria de imagens
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 40,
+        autoplay: true,           
+        autoplayTimeout: 3000,    
+        responsive: {
+            0: { items: 1 },      
+            600: { items: 2 },    
+            1000: { items: 4 }    
+        }
+    });
+});
+
+
